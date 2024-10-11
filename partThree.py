e921a1d100ad201d27675eb6aa0bbdb8a821a33a
Risha-3 <rs989@kent.ac.uk>
2024-10-11 15:12:47 +0100

    Completed task 3

--- partThree.py
+++ partThree.py
@@ -1,1 +1,14 @@
+score = int(input("What score did you get? "))
+if 90 <= score <= 100:
+    print("Grade A")
+elif 80 <= score < 90:
+    print("Grade B")
+elif 70 <= score < 80:
+    print("Grade C")
+elif 60 <= score < 70:
+    print("Grade D")
+elif 0 <= score < 60:
+    print("Input must be a number from 1-100. PLease try again.")
+
+
 
